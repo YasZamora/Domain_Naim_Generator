93c74df26a0abd82b9d10ea2acfc9151bb790ec2
YasZamora <yzamo001@gmail.com>
2022-01-15 18:00:42 +0000

    Code Domain Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  for (let k = 0; k < 2; k++) {
+    for (let j = 0; j < 2; j++) {
+      for (let i = 0; i < 2; i++) {
+        console.log(pronoun[k] + adj[j] + noun[i] + ".com");
+      }
+    }
+  }
 };
